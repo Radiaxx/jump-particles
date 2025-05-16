0ebbcb34d1bc0efe6ecebca5c09ecf0c47e26d94
Radiaxx <radiax@example.com>
2025-05-16 16:36:03 +0200

    Added SFML template.

---
+++ SFML window setup/src/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({720u, 480u}), "CMake SFML Project");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
